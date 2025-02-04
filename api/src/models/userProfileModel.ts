6183a0821666fdf7fcb2bba9fc9615613c64996e
J <jpham26@gmail.com>
2025-02-04 11:17:18 -0800

    refactor: improve type safety using Prisma's built-in types

--- api/src/models/userProfileModel.ts
+++ api/src/models/userProfileModel.ts
@@ -1,13 +1,37 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
-interface UpdateProfileData {
-  displayName?: string;
-  bio?: string;
-  imageUrl?: string;
-}
+// interface UpdateProfileData {
+//   displayName?: string;
+//   bio?: string;
+//   imageUrl?: string;
+// }
 
-export const getByUsername = async (username: string) => {
+type UserProfileWithDetails = Prisma.UserGetPayload<{
+  select: {
+    id: true,
+    username: true
+    profile: {
+      select: {
+        displayName: true,
+        bio: true,
+        imageUrl: true,
+      },
+    },
+  },
+}>;
+
+type ProfileDetails = Prisma.UserProfileGetPayload<{
+  select: {
+    displayName: true,
+    bio: true,
+    imageUrl: true,
+  },
+}>;
+
+export const getByUsername = async (
+  username: string
+): Promise<UserProfileWithDetails | null> => {
   return prisma.user.findUnique({
     where: { username },
     select: {
@@ -17,25 +41,30 @@ export const getByUsername = async (username: string) => {
         select: {
           displayName: true,
           bio: true,
-          imageUrl: true
-        }
-      }
-    }
+          imageUrl: true,
+        },
+      },
+    },
   });
 };
 
-export const getByUserId = async (userId: string) => {
+export const getByUserId = async (
+  userId: string
+): Promise<ProfileDetails | null> => {
   return prisma.userProfile.findUnique({
     where: { userId },
       select: {
         displayName: true,
         bio: true,
-        imageUrl: true
-      }
+        imageUrl: true,
+      },
   });
 };
 
-export const update = async (userId: string, data: UpdateProfileData) => {
+export const update = async (
+  userId: string,
+  data: Prisma.UserProfileUpdateInput
+): Promise<ProfileDetails | null> => {
   return prisma.userProfile.update({
     where: { userId },
     data,
@@ -43,7 +72,7 @@ export const update = async (userId: string, data: UpdateProfileData) => {
       displayName: true,
       bio: true,
       imageUrl: true
-    }
+    },
   });
 } 
 
