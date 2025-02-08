24506eaeb618299782aee07119ffcc5691514316
J <jpham26@gmail.com>
2025-02-07 17:18:05 -0800

    init commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Messaging App</h1>
+      </div>
+    </Router>
+  )
+}
+
+export default App
